e1f799e6f30747d2832318c273f61411a2d78dd9
Pooja Giri <poojagiri@Poojas-MBP.lan>
2026-01-11 18:35:22 -0600

    hello

---
+++ Add.java
@@ -0,0 +1,12 @@
+public class Add{
+
+static int x=5;
+static int y=6;
+
+public static void main(String[]args){
+System.out.println(x+y);
+
+
+}
+
+}
--- test4.java
+++ test4.java
@@ -3,7 +3,5 @@ class test4{
 public static void main (String[]args){
 System.out.println("Hello Universe");
 
-
-
 }
 }
